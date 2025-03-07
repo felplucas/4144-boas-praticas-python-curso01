94a3510b1d44a6ef776602d5f8e6ce4d73a31c4d
Lucas Felipe Patera <lucasfelipepatera@gmail.com>
2025-03-07 00:20:29 -0300

    Update routers_produtos.py

--- app/routers/routers_produtos.py
+++ app/routers/routers_produtos.py
@@ -92,6 +92,7 @@ def recomendar_produtos(usuario_id: int, preferencias: Preferencias) -> List[Pro
     Returns:
         List[Produto]: Uma lista de produtos recomendados com base no histórico de compras e preferências.
     """
+
     if usuario_id not in historico_de_compras:
         raise HTTPException(
             status_code=404, detail="Histórico de compras não encontrado"
@@ -109,13 +110,15 @@ def recomendar_produtos(usuario_id: int, preferencias: Preferencias) -> List[Pro
     ]
 
     # Filtrar as recomendações com base nas preferências
-    produtos_recomendados = [
-        p for p in produtos_recomendados if p.categoria in preferencias.categorias
+    produtos_recomendados_categorias = [
+        produto for produto in produtos_recomendados if produto.categoria in preferencias.categorias
     ]  # Preferencias de categorias
-    produtos_recomendados = [
-        p
-        for p in produtos_recomendados
-        if any(tag in preferencias.tags for tag in p.tags)
-    ]  # Preferencias de tags
 
-    return produtos_recomendados
+    produtos_recomendados_filtrados = []
+    for produto in produtos_recomendados_categorias:
+        for tag in produto.tags:
+            if tag in preferencias.tags:
+                produtos_recomendados_filtrados.append(produto)
+                break
+
+    return produtos_recomendados_filtrados
